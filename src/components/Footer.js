981ae41ff9cfd02f9f3f9049a7fa23f0e4e3cac7
Jawad <Jawad.Nazari@outlook.com>
2023-05-31 00:41:05 +0500

    Adding footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="dim-bg">
+    <p className="copyright-text">
+      © 2023 Jawad Nazari CCT App. All rights reserved.
+    </p>
+  </footer>
+);
+
+export default Footer;
